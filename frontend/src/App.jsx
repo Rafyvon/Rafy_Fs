6b3d513f7ca7f1f9a3add0390d816eca776db0d7
Rafyvon <rafaelvonneramos@gmail.com>
2025-12-06 14:37:44 +0800

    4th day

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,38 @@
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
-import Home from './pages/Home.jsx'
-import Products from './pages/Products.jsx';
-
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import React from "react";
+import Home from "./pages/Home.jsx";
+import Products from "./pages/Products.jsx";
+import NotFound from "./pages/NotFound.jsx";
+import Login from "./pages/Login.jsx";
+import Profile from "./pages/Profile.jsx";
+import Header from "./components/Header.jsx";
+import { AuthProvider } from "./context/AuthProvider.jsx";
+import { PrivateRoute } from "./context/PrivateRoute.jsx";
 
 const App = () => {
   return (
-    <>
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/products" element={<Products/>}/>
-    
-    </Routes>
-    </Router>
-    </>
-)
-}
+    <div>
+      <Router>
+        <AuthProvider>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/best-products" element={<Products />} />
+            <Route
+              path="/profile"
+              element={
+                <PrivateRoute>
+                  <Profile />
+                </PrivateRoute>
+              }
+            />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </AuthProvider>
+      </Router>
+    </div>
+  );
+};
 
 export default App;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import Hero from '../components/Hero.jsx'
-import Header from '../components/Header.jsx'
 import ProductGuide from '../components/ProductGuide.jsx'
 import ProductList from '../components/ProductList.jsx'
 import Loading from '../components/Loading.jsx'
@@ -8,7 +7,7 @@ import Loading from '../components/Loading.jsx'
 const Home = () => {
   return (
     <div>
-        <Header/>
+        
         <Hero/>
         <ProductGuide/>
         <ProductList/>
